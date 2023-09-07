7d6b17078b0640bd29009f3bc8d31b741ace7782
Admin <admin@Admins-Mac-mini.local>
2023-09-07 19:54:09 +0530

    bug fixes

--- src/Components/UserDetail.js
+++ src/Components/UserDetail.js
@@ -54,7 +54,8 @@ const UserDetail = ({
 
   const onWhatsapp = async () => {
     const vendorPhoneNumber = data?.vendor?.phone_no.replace(/\s/g, "") || data?.order?.phone_number.replace(/\s/g, "")
-    let url = `whatsapp://send?phone=${data?.vendor?.dial_code}${vendorPhoneNumber}`;
+    let url = `whatsapp://send?phone=${vendorPhoneNumber}`;
+    console.log(url,'url0url')
     Linking.openURL(url)
       .then((data) => {
         console.log("WhatsApp Opened successfully " + data); //<---Success
